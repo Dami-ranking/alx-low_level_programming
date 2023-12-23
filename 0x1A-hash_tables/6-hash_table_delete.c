5ade18c1fbd6388e0765ce66b16fef3348cc449e
Dami-ranking <ezeomedamian042@gmail.com>
2023-12-22 18:11:28 -0800

    all

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,21 +7,21 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
-	unsigned long int j;
+	hash_node_t *node, *tmpo;
+	unsigned long int i;
 
-	for (j = 0; j < ht->size; j++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[j] != NULL)
+		if (ht->array[i] != NULL)
 		{
-			node = ht->array[j];
+			node = ht->array[i];
 			while (node != NULL)
 			{
-				tmp = node->next;
+				tmpo = node->next;
 				free(node->key);
 				free(node->value);
 				free(node);
-				node = tmp;
+				node = tmpo;
 			}
 		}
 	}
